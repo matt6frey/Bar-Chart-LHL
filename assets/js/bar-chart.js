bf0c6caa2706ac5338b1aeda5224dd9f49562d73
Matt Frey <matt6frey@gmail.com>
2018-04-29 00:08:31 +0000

    Updated Index.html with 'DOCTYPE' declaration and UTF-8 charset, updated bar-chart.js with further error checking: ensure that data within the data array contains only numbers.

--- assets/js/bar-chart.js
+++ assets/js/bar-chart.js
@@ -4,19 +4,30 @@
 // 'options' parameter is an object that has options pertaining to the chart itelf.
 // 'element' parameter determines which element to display the chart in. (Could be 'element' or 'element.class' or element#id)
 
-function drawBarChart(data, options, element) {
+function drawBarChart(data, options, element) { // Draw Chart.
   //Error Checks on initial inputs.
   var errorCheck = [[jQuery.type(data) === 'array', 'array', data], [jQuery.type(options) === 'object', 'object', options], [jQuery.type(element) === 'string', 'string', element]];
-
+  var alphabetRegExp = /[A-Za-z]/;
   //If Errors exist, report them.
   for (var i in errorCheck) {
     if(errorCheck[i][0] === false) {
       var param = Number(i)+1;
-      console.log("Parameter " + param + " expects a " + errorCheck[i][1] + ", instead a " + jQuery.type(errorCheck[i][2]) + " was given.");
-      return "false";
+      console.log("Error: Parameter " + param + " expects a " + errorCheck[i][1] + ", instead a " + jQuery.type(errorCheck[i][2]) + " was given.");
+      return false;
     }
   }
+  var dataCheck = data.map( x => typeof x );
 
+  if(dataCheck.includes('string')) {
+    console.log("Error: Parameter 1 only accepts an array that contains numerical values.");
+    return false;
+  }
+
+
+
+}
+
+function setOptions (options) {
 
 }
 
@@ -25,4 +36,5 @@ function drawBarChart(data, options, element) {
 console.log(drawBarChart('data', {"name":"object"}, 'body'));
 console.log(drawBarChart([1,2,3], 'options', 'element'));
 console.log(drawBarChart([1,2,3], {"name":"object"}, 'element'));
+console.log(drawBarChart(['a',2,3], {"name":"object"}, 'element'));
 */
